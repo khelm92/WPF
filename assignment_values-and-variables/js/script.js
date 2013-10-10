8a976927bb0d73fe0d289a0b3c653a3824f24c63
Kasey Helm <khelm92@fullsail.edu>
2013-10-09 20:42:58 -0700

    values and variables

---
+++ assignment_values-and-variables/js/script.js
@@ -0,0 +1,16 @@
+// Javascript
+
+var yearBorn = 1992;
+/*
+console.log(yearBorn);
+console.log("Hello World!");
+*/
+
+alert(yearBorn);
+
+var isStudent = false;
+
+var firstName = "Kasey";
+
+var friendNames = ["Brian", "Nick", "Daniel", "Kody"]
+console.log(friendNames[0]);
