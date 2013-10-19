324b3e1789791a8f642388fc7b7368d86444dacf
Kasey Helm <khelm92@fullsail.edu>
2013-10-18 20:00:43 -0700

    Conditionals - Wacky Done

--- conditionals_wacky/js/script.js
+++ conditionals_wacky/js/script.js
@@ -1,10 +1,16 @@
-// conditionals
+// Kasey Helm 10/18/2013 Conditionals - Wacky
 
-var age = 11;
-var book;
+//what type of litter should I buy for my cats
 
-book = (age < 10) ? "Green Eggs & Ham" : "The Time Machine";
-console.log(book);
+var numOfCats = prompt("How many cats do you have? \n*Numerical Values Only*");
+if(numOfCats > 1){alert("Get multi-cat litter");
+}else if(numOfCats == 1){var stinkyCat = prompt("Does your litter box stink?");
+}
+
+var results;
+results = (stinkyCat == "yes") ? alert("Get anti-odor litter") : alert("Get the cheapest clumping litter available");
+results = (stinkyCat == "yes") ? console.log("Get anti-odor litter") : console.log("Get the cheapest clumping litter available");
+
 
 
 
