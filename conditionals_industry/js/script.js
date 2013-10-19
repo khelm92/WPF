901d8e0d3a118071ff37dc4da41a282efaef8e4b
Kasey Helm <khelm92@fullsail.edu>
2013-10-18 19:06:36 -0700

    Industry Conditionals done

--- conditionals_industry/js/script.js
+++ conditionals_industry/js/script.js
@@ -1,10 +1,17 @@
-// conditionals
+// Kasey Helm 10/18/2013 Conditionals Assignment - Industry
 
-var age = 11;
-var book;
+//in my expressions assignment, I saw how much someone would save with a good driver discount on their auto insurance policy, this time I'm going to see if they are even eligible to get the discount
 
-book = (age < 10) ? "Green Eggs & Ham" : "The Time Machine";
-console.log(book);
+
+var drivingYears = prompt("How many years have you had your driver's license\n*Numerical Values Only, no letters*"); //check how many years the user has had a drivers license
+if(drivingYears >= 3){var tickets = prompt("Do you have any tickets or accidents on your record?"); //if more than or equal to 3 years, continue to this question
+}else{alert("Sorry, you're not eligible for the good driver discount"); // if less than 3 years, denied
+}
+
+var results;
+results = (tickets == "no") ? alert("Congratulations, you are eligible for the \ngood driver discount!") : alert("Sorry, you're not eligible for the good driver discount"); //if they have no tickets or accidents, they can get the discount. if they do, they are not eligible
+results = (tickets == "no") ? console.log("Congratulations, you are eligible for the \ngood driver discount!") : console.log("Sorry, you're not eligible for the good driver discount"); //same as above only printed in console
+
 
 
 
